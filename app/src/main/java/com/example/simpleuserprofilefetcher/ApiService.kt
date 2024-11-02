2673ed3ad8fc759a9b11c5489430e1845e43ac8a
HandsomeRobot <alaminjoy456@gmail.com>
2024-11-02 11:46:04 +0600

    done

---
+++ app/src/main/java/com/example/simpleuserprofilefetcher/ApiService.kt
@@ -0,0 +1,10 @@
+package com.example.simpleuserprofilefetcher
+
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface ApiService {
+    @GET("users/1")
+    fun getUserProfile(): Call<UserProfile>
+}
